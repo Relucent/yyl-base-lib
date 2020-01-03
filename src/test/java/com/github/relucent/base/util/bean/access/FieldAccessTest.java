4f7b6bd1bcd4f598c8703c677546e040c55c36e2
yyl <relucent@163.com>
2020-01-03 15:30:42 +0800

    +

---
+++ src/test/java/com/github/relucent/base/util/bean/access/FieldAccessTest.java
@@ -0,0 +1,21 @@
+package com.github.relucent.base.util.bean.access;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FieldAccessTest {
+
+    @Test
+    public void testGetSet() {
+        FieldAccess access = FieldAccess.create(Sample.class);
+        Sample sample = new Sample();
+        sample.value = "A";
+        Assert.assertEquals(access.get(sample, "value"), "A");
+        access.set(sample, "value", "B");
+        Assert.assertEquals(sample.value, "B");
+    }
+
+    private static class Sample {
+        public String value;
+    }
+}
---
+++ src/test/java/com/github/relucent/base/util/bean/access/MethodAccessTest.java
@@ -0,0 +1,30 @@
+package com.github.relucent.base.util.bean.access;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MethodAccessTest {
+
+    @Test
+    public void testGetSet() {
+        MethodAccess access = MethodAccess.create(Sample.class);
+        Sample sample = new Sample();
+        sample.setValue("A");
+        Assert.assertEquals(access.invoke(sample, "getValue", new Class[0]), "A");
+        access.invoke(sample, "setValue", new Class[] {String.class}, "B");
+        Assert.assertEquals(sample.getValue(), "B");
+    }
+
+    private static class Sample {
+
+        private String value;
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+    }
+}
