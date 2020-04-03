7002ef8e0e781e5ffc7e8bfe2d8ef34f1c4e3b8b
yyl <relucent@163.com>
2020-04-03 20:59:27 +0800

    #

---
+++ src/test/java/com/github/relucent/base/common/exception/ExceptionHelperTest.java
@@ -0,0 +1,27 @@
+package com.github.relucent.base.common.exception;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ExceptionHelperTest {
+
+	@Test(expected = GeneralException.class)
+	public void testError() {
+		throw ExceptionHelper.error("error");
+	}
+
+	@Test(expected = PromptException.class)
+	public void testPrompt() {
+		throw ExceptionHelper.prompt("prompt");
+	}
+
+	@Test
+	public void testExceptionMessage() {
+		String message = "hello";
+		try {
+			throw ExceptionHelper.prompt(message);
+		} catch (Exception e) {
+			Assert.assertEquals(message, e.getMessage());
+		}
+	}
+}
