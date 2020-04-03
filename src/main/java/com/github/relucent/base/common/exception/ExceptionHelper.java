5bbc75f44ad584f4aff05c2ff053914042169eed
yyl <relucent@163.com>
2020-04-03 20:59:27 +0800

    #

--- src/main/java/com/github/relucent/base/common/exception/ExceptionHelper.java
+++ src/main/java/com/github/relucent/base/common/exception/ExceptionHelper.java
@@ -6,44 +6,44 @@ package com.github.relucent.base.common.exception;
  */
 public class ExceptionHelper {
 
-    /**
-     * 创建通用异常
-     * @param cause 源异常
-     * @return 通用异常
-     */
-    public static GeneralException propagate(Throwable cause) {
-        if (cause instanceof GeneralException) {
-            return (GeneralException) cause;
-        }
-        return new GeneralException(ErrorType.DEFAULT, "#", cause);
-    }
+	/**
+	 * 创建通用异常
+	 * @param cause 源异常
+	 * @return 通用异常
+	 */
+	public static GeneralException propagate(Throwable cause) {
+		if (cause instanceof GeneralException) {
+			return (GeneralException) cause;
+		}
+		return new GeneralException("#", cause);
+	}
 
-    /**
-     * 创建未捕获异常
-     * @param message 异常信息
-     * @param cause 源异常
-     * @return 未捕获异常
-     */
-    public static GeneralException propagate(String message, Throwable cause) {
-        return new GeneralException(ErrorType.DEFAULT, message, cause);
-    }
+	/**
+	 * 创建未捕获异常
+	 * @param message 异常信息
+	 * @param cause 源异常
+	 * @return 未捕获异常
+	 */
+	public static GeneralException propagate(String message, Throwable cause) {
+		return new GeneralException(message, cause);
+	}
 
-    /**
-     * 创建提示异常
-     * @param message 提示消息
-     * @return 提示异常
-     */
-    public static GeneralException prompt(String message) {
-        return new GeneralException(ErrorType.PROMPT, message);
-    }
+	/**
+	 * 创建提示异常
+	 * @param message 提示消息
+	 * @return 提示异常
+	 */
+	public static PromptException prompt(String message) {
+		return new PromptException(message);
+	}
 
-    /**
-     * 创建通用异常
-     * @param type 异常类型
-     * @param message 异常信息
-     * @return 通用异常
-     */
-    public static GeneralException error(ErrorType type, String message) {
-        return new GeneralException(type, message);
-    }
+	/**
+	 * 创建通用异常
+	 * @param type 异常类型
+	 * @param message 异常信息
+	 * @return 通用异常
+	 */
+	public static GeneralException error(String message) {
+		return new GeneralException(message);
+	}
 }
---
+++ src/main/java/com/github/relucent/base/common/exception/PromptException.java
@@ -0,0 +1,17 @@
+package com.github.relucent.base.common.exception;
+
+/**
+ * 提示异常类
+ * @see RuntimeException
+ * @author YYL
+ */
+@SuppressWarnings("serial")
+public class PromptException extends GeneralException {
+	/**
+	 * 构造函数
+	 * @param message 异常的详细信息， 可以使用{@link #getMessage()} 方法获取
+	 */
+	public PromptException(String message) {
+		super(message);
+	}
+}
