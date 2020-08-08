2485d5910d459191115bd4b59a31670c6d2d1818
yyl <relucent@163.com>
2020-08-08 14:37:50 +0800

    #

--- src/main/java/com/github/relucent/base/common/script/ScriptUtil.java
+++ src/main/java/com/github/relucent/base/common/script/ScriptUtil.java
@@ -1,25 +1,49 @@
 package com.github.relucent.base.common.script;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 /**
  * 脚本工具类
  * @author YYL
  */
 public class ScriptUtil {
 
-    /**
-     * 获得脚本引擎实例
-     * @param name 脚本名称
-     * @return 脚本引擎实例
-     */
-    public static ScriptEnginex getScriptEngine(String name) {
-        return new ScriptEnginex(name);
-    }
+	/**
+	 * 获得脚本引擎实例
+	 * @param name 脚本名称
+	 * @return 脚本引擎实例
+	 */
+	public static ScriptEnginex getScriptEngine(String name) {
+		ScriptEngine engine = getEngine(name);
+		return new ScriptEngineWrapper(engine);
+	}
 
-    /**
-     * 获得 JavaScript 脚本引擎实例
-     * @return JavaScript 脚本引擎实例
-     */
-    public static JavaScriptEngine getJavaScriptEngine() {
-        return new JavaScriptEngine();
-    }
+	/**
+	 * 获得 JavaScript 脚本引擎实例
+	 * @return JavaScript 脚本引擎实例
+	 */
+	public static ScriptEnginex getJavaScriptEngine() {
+		return getScriptEngine("javascript");
+	}
+
+	/**
+	 * 根据名称获得脚本引擎
+	 * @param name 脚本引擎名称
+	 * @return 脚本引擎
+	 */
+	private static ScriptEngine getEngine(String name) {
+		ScriptEngineManager manager = new ScriptEngineManager();
+		ScriptEngine engine = manager.getEngineByName(name);
+		if (engine == null) {
+			engine = manager.getEngineByExtension(name);
+		}
+		if (engine == null) {
+			engine = manager.getEngineByMimeType(name);
+		}
+		if (engine == null) {
+			throw new NullPointerException("Script for [" + name + "] not support !");
+		}
+		return engine;
+	}
 }
