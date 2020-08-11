165363881d67980ef38193d534665b7a47048b33
yyl <relucent@163.com>
2020-08-11 15:27:51 +0800

    +

--- src/main/java/com/github/relucent/base/common/identifier/TimeIdWorker.java
+++ src/main/java/com/github/relucent/base/common/identifier/TimeIdWorker.java
@@ -11,122 +11,123 @@ import java.time.format.DateTimeFormatter;
  */
 public class TimeIdWorker {
 
-    /** 日期格式化 */
-    public static final DateTimeFormatter DATETIME_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
+	/** 默认实例 */
+	public static final TimeIdWorker DEFAULT = new TimeIdWorker();
 
-    /** 秒内序列限制 */
-    private final long MAX_SEQUENCE = 99999;
-    /** 秒内序列限制 */
-    private final int MAX_SEQUENCE_SIZE = Long.toString(MAX_SEQUENCE).length();
-    /** "0" 字符 */
-    private final char ZERO_CHAR = '0';
+	/** 日期格式化 */
+	private static final DateTimeFormatter DATETIME_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
 
-    /** 序列ID前缀(为不同业务设置不同前缀，可以防止集群环境序列ID的冲突) */
-    private final String prefix;
+	/** 秒内序列限制 */
+	private final long MAX_SEQUENCE = 99999;
+	/** 秒内序列限制 */
+	private final int MAX_SEQUENCE_SIZE = Long.toString(MAX_SEQUENCE).length();
+	/** "0" 字符 */
+	private final char ZERO_CHAR = '0';
 
-    /** 序列ID后缀(为不同服务器设置不同后缀，可以防止集群环境序列ID的冲突) */
-    private final String suffix;
+	/** 序列ID前缀(为不同业务设置不同前缀，可以防止集群环境序列ID的冲突) */
+	private final String prefix;
 
-    /** 上次生成ID的时间截(秒) */
-    private long lastSeconds = -1L;
+	/** 序列ID后缀(为不同服务器设置不同后缀，可以防止集群环境序列ID的冲突) */
+	private final String suffix;
 
-    /** 日期时间字符串 */
-    private String datetimeString = "00000000000000";
+	/** 上次生成ID的时间截(秒) */
+	private long lastSeconds = -1L;
 
-    /** 秒内序列 */
-    private long sequence = 0L;
+	/** 日期时间字符串 */
+	private String datetimeString = "00000000000000";
 
-    /**
-     * 构造函数
-     */
-    public TimeIdWorker() {
-        this.prefix = "";
-        this.suffix = "";
-    }
+	/** 秒内序列 */
+	private long sequence = 0L;
 
-    /**
-     * 构造函数
-     * @param prefix 序列ID前缀
-     * @param suffix 序列ID后缀
-     */
-    public TimeIdWorker(String prefix, String suffix) {
-        this.prefix = prefix;
-        this.suffix = suffix;
-    }
+	/**
+	 * 构造函数
+	 */
+	public TimeIdWorker() {
+		this.prefix = "";
+		this.suffix = "";
+	}
 
-    /**
-     * 获得下一个序列ID
-     * @return 序列ID
-     */
-    public synchronized String nextId() {
+	/**
+	 * 构造函数
+	 * @param prefix 序列ID前缀
+	 * @param suffix 序列ID后缀
+	 */
+	public TimeIdWorker(String prefix, String suffix) {
+		this.prefix = prefix;
+		this.suffix = suffix;
+	}
 
-        long seconds = secondsGen();
+	/**
+	 * 获得下一个序列ID
+	 * @return 序列ID
+	 */
+	public synchronized String nextId() {
 
-        // 如果当前时间小于上一次ID生成的时间戳，说明系统时钟回退过这个时候应当抛出异常
-        if (seconds < lastSeconds) {
-            throw new RuntimeException(String.format("Clock moved backwards.  Refusing to generate id for %d seconds",
-                    lastSeconds - seconds));
-        }
+		long seconds = secondsGen();
 
-        // 如果是同一秒生成的，则递增秒内序列
-        if (lastSeconds == seconds) {
-            sequence++;
-            // 秒内序列溢出，需要等待下一秒
-            if (sequence > MAX_SEQUENCE) {
-                seconds = tilNextSeconds(lastSeconds);
-            }
-        }
+		// 如果当前时间小于上一次ID生成的时间戳，说明系统时钟回退过这个时候应当抛出异常
+		if (seconds < lastSeconds) {
+			throw new RuntimeException(String.format("Clock moved backwards.  Refusing to generate id for %d seconds", lastSeconds - seconds));
+		}
 
+		// 如果是同一秒生成的，则递增秒内序列
+		if (lastSeconds == seconds) {
+			sequence++;
+			// 秒内序列溢出，需要等待下一秒
+			if (sequence > MAX_SEQUENCE) {
+				seconds = tilNextSeconds(lastSeconds);
+			}
+		}
 
-        // 时间戳改变，秒内序列重置
-        if (lastSeconds != seconds) {
-            lastSeconds = seconds;
-            sequence = 0L;
-            datetimeString = formatDatetimeOfSeconds(lastSeconds);
-        }
+		// 时间戳改变，秒内序列重置
+		if (lastSeconds != seconds) {
+			lastSeconds = seconds;
+			sequence = 0L;
+			datetimeString = formatDatetimeOfSeconds(lastSeconds);
+		}
 
-        // 拼装 ID字符串
-        StringBuffer buffer = new StringBuffer();
-        buffer.append(prefix);
-        buffer.append(datetimeString);
-        String appendSequence = Long.toString(sequence);
-        for (int i = MAX_SEQUENCE_SIZE - appendSequence.length(); i > 0; i--) {
-            buffer.append(ZERO_CHAR);
-        }
-        buffer.append(appendSequence);
-        buffer.append(suffix);
-        return buffer.toString();
-    }
+		// 拼装 ID字符串
+		StringBuffer buffer = new StringBuffer();
+		buffer.append(prefix);
+		buffer.append(datetimeString);
+		String appendSequence = Long.toString(sequence);
+		for (int i = MAX_SEQUENCE_SIZE - appendSequence.length(); i > 0; i--) {
+			buffer.append(ZERO_CHAR);
+		}
+		buffer.append(appendSequence);
+		buffer.append(suffix);
+		return buffer.toString();
+	}
 
-    /**
-     * 阻塞到下一个秒，直到获得新的时间戳(秒)
-     * @param lastSecond 上次生成ID的时间戳(秒)
-     * @return 当前时间戳(秒)
-     */
-    private long tilNextSeconds(long lastSeconds) {
-        long seconds = secondsGen();
-        while (seconds <= lastSeconds) {
-            seconds = secondsGen();
-        }
-        return seconds;
-    }
+	/**
+	 * 阻塞到下一个秒，直到获得新的时间戳(秒)
+	 * @param lastSecond 上次生成ID的时间戳(秒)
+	 * @return 当前时间戳(秒)
+	 */
+	private long tilNextSeconds(long lastSeconds) {
+		long seconds = secondsGen();
+		while (seconds <= lastSeconds) {
+			seconds = secondsGen();
+		}
+		return seconds;
+	}
 
-    /**
-     * 返回以秒为单位的当前时间
-     * @return 当前时间(秒)
-     */
-    private long secondsGen() {
-        return System.currentTimeMillis() / 1000L;
-    }
+	/**
+	 * 返回以秒为单位的当前时间
+	 * @return 当前时间(秒)
+	 */
+	private long secondsGen() {
+		return System.currentTimeMillis() / 1000L;
+	}
 
-    /**
-     * 格式化时间秒为日期时间字符串
-     * @param seconds 时间秒
-     * @return 日期时间字符串
-     */
-    private String formatDatetimeOfSeconds(long seconds) {
-        Instant instant = Instant.ofEpochMilli(seconds * 1000L);
-        ZoneId zone = ZoneId.systemDefault();
-        return DATETIME_FORMATTER.format(LocalDateTime.ofInstant(instant, zone));
-    }
+	/**
+	 * 格式化时间秒为日期时间字符串
+	 * @param seconds 时间秒
+	 * @return 日期时间字符串
+	 */
+	private String formatDatetimeOfSeconds(long seconds) {
+		Instant instant = Instant.ofEpochMilli(seconds * 1000L);
+		ZoneId zone = ZoneId.systemDefault();
+		return DATETIME_FORMATTER.format(LocalDateTime.ofInstant(instant, zone));
+	}
 }
