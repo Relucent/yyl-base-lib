9e6e2de747dd751391e1a12be47595014155d862
yyl <relucent@163.com>
2020-10-18 16:30:26 +0800

    +

---
+++ src/main/java/com/github/relucent/base/plugin/mybatis/PageContext.java
@@ -0,0 +1,35 @@
+package com.github.relucent.base.plugin.mybatis;
+
+import java.io.Serializable;
+
+@SuppressWarnings("serial")
+public class PageContext implements Serializable {
+
+    private long offset = 0L;
+    private long limit = 1L;
+    private long total = -1L;
+
+    public long getOffset() {
+        return offset;
+    }
+
+    public void setOffset(long offset) {
+        this.offset = offset;
+    }
+
+    public long getLimit() {
+        return limit;
+    }
+
+    public void setLimit(long limit) {
+        this.limit = limit;
+    }
+
+    public long getTotal() {
+        return total;
+    }
+
+    public void setTotal(long total) {
+        this.total = total;
+    }
+}
