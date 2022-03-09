b5e551eb6b635be7a63cf3470b5aed587421bfef
yyl <relucent@163.com>
2022-03-09 13:50:45 +0800

    +

---
+++ src/test/java/com/github/relucent/base/common/regex/RegexUtilTest.java
@@ -0,0 +1,17 @@
+package com.github.relucent.base.common.regex;
+
+import java.util.regex.Pattern;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class RegexUtilTest {
+    @Test
+    public void testReplaceAll() {
+        String actual = RegexUtil.replaceAll("A1B2C3D4", Pattern.compile("\\d+"),
+                matcher -> Integer.toString(Integer.parseInt(matcher.group(0)) * 2));
+        String expected = "A2B4C6D8";
+        System.out.println(actual);
+        Assert.assertEquals(expected, actual);
+    }
+}
