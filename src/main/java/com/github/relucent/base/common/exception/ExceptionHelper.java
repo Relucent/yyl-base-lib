2aa4960e4bef77b4981f3de14c0f80122693207c
yyl <relucent163.com>
2022-06-26 01:04:51 +0800

    #

--- src/main/java/com/github/relucent/base/common/exception/ExceptionHelper.java
+++ src/main/java/com/github/relucent/base/common/exception/ExceptionHelper.java
@@ -1,68 +1,82 @@
 package com.github.relucent.base.common.exception;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 /**
  * 异常工具类
  * @author YYL
  */
 public class ExceptionHelper {
 
-    /**
-     * 创建通用异常
-     * @param cause 源异常
-     * @return 通用异常
-     */
-    public static GeneralException propagate(Throwable cause) {
-        if (cause instanceof GeneralException) {
-            return (GeneralException) cause;
-        }
-        return new GeneralException("#", cause);
-    }
+	/**
+	 * 创建通用异常
+	 * @param cause 源异常
+	 * @return 通用异常
+	 */
+	public static GeneralException propagate(Throwable cause) {
+		if (cause instanceof GeneralException) {
+			return (GeneralException) cause;
+		}
+		return new GeneralException("#", cause);
+	}
 
-    /**
-     * 创建未捕获异常
-     * @param message 异常信息
-     * @param cause 源异常
-     * @return 未捕获异常
-     */
-    public static GeneralException propagate(String message, Throwable cause) {
-        return new GeneralException(message, cause);
-    }
+	/**
+	 * 创建未捕获异常
+	 * @param message 异常信息
+	 * @param cause 源异常
+	 * @return 未捕获异常
+	 */
+	public static GeneralException propagate(String message, Throwable cause) {
+		return new GeneralException(message, cause);
+	}
 
-    /**
-     * 创建提示异常
-     * @param message 提示消息
-     * @return 提示异常
-     */
-    public static PromptException prompt(String message) {
-        return new PromptException(message);
-    }
+	/**
+	 * 创建提示异常
+	 * @param message 提示消息
+	 * @return 提示异常
+	 */
+	public static PromptException prompt(String message) {
+		return new PromptException(message);
+	}
 
-    /**
-     * 创建提示异常
-     * @param code 异常编码
-     * @param message 提示消息
-     * @return 提示异常
-     */
-    public static PromptException prompt(Integer code, String message) {
-        return new PromptException(message);
-    }
+	/**
+	 * 创建提示异常
+	 * @param code 异常编码
+	 * @param message 提示消息
+	 * @return 提示异常
+	 */
+	public static PromptException prompt(Integer code, String message) {
+		return new PromptException(message);
+	}
 
-    /**
-     * 创建通用异常
-     * @param message 异常信息
-     * @return 通用异常
-     */
-    public static GeneralException error(String message) {
-        return new GeneralException(message);
-    }
+	/**
+	 * 创建通用异常
+	 * @param message 异常信息
+	 * @return 通用异常
+	 */
+	public static GeneralException error(String message) {
+		return new GeneralException(message);
+	}
 
-    /**
-     * 创建通用异常
-     * @param code 异常编码
-     * @param message 异常信息
-     * @return 通用异常
-     */
-    public static GeneralException error(Integer code, String message) {
-        return new GeneralException(code, message);
-    }
+	/**
+	 * 创建通用异常
+	 * @param code 异常编码
+	 * @param message 异常信息
+	 * @return 通用异常
+	 */
+	public static GeneralException error(Integer code, String message) {
+		return new GeneralException(code, message);
+	}
+
+	/**
+	 * 输出异常堆栈字符串
+	 * @param throwable 异常
+	 * @return 异常堆栈字符串
+	 */
+	public static String getStackTraceAsString(Throwable throwable) {
+		StringWriter stringWriter = new StringWriter();
+		throwable.printStackTrace(new PrintWriter(stringWriter));
+		return stringWriter.toString();
+	}
 }
--- src/main/java/com/github/relucent/base/plugin/gson/GsonHandler.java
+++ src/main/java/com/github/relucent/base/plugin/gson/GsonHandler.java
@@ -14,112 +14,115 @@ import com.google.gson.JsonParser;
 
 public class GsonHandler implements JsonHandler {
 
-    // ===================================Fields==============================================
-    public static final GsonHandler INSTANCE = new GsonHandler();
-    private final Logger logger = Logger.getLogger(getClass());
-    private final Gson gson;
+	// ===================================Fields==============================================
+	public static final GsonHandler INSTANCE = new GsonHandler();
+	private final Logger logger = Logger.getLogger(getClass());
+	private final Gson gson;
 
-    // ===================================Constructors========================================
+	// ===================================Constructors========================================
 
-    /**
-     * 构造函数
-     */
-    public GsonHandler() {
-        this(new GsonBuilder().create());
-    }
+	/**
+	 * 构造函数
+	 */
+	public GsonHandler() {
+		this(new GsonBuilder().create());
+	}
 
-    /**
-     * 构造函数使用指定 {@link Gson}
-     * @param gson {@link Gson}
-     */
-    public GsonHandler(Gson gson) {
-        this.gson = gson;
-    }
+	/**
+	 * 构造函数使用指定 {@link Gson}
+	 * @param gson {@link Gson}
+	 */
+	public GsonHandler(Gson gson) {
+		this.gson = gson;
+	}
 
-    // ===================================Methods=============================================
-    /**
-     * 将JAVA对象编码为JSON字符串
-     * @param src JAVA对象
-     * @return 对象的JSON字符串
-     */
-    public String encode(Object src) {
-        return gson.toJson(src);
-    }
+	// ===================================Methods=============================================
+	/**
+	 * 将JAVA对象编码为JSON字符串
+	 * @param src JAVA对象
+	 * @return 对象的JSON字符串
+	 */
+	public String encode(Object src) {
+		return gson.toJson(src);
+	}
 
-    /**
-     * 将JSON字符串解码为JAVA对象
-     * @param <T> 对象泛型
-     * @param json 对象的JSON字符串
-     * @param type JAVA对象类型
-     * @return JSON对应的JAVA对象，如果无法解析将返回NULL.
-     */
-    public <T> T decode(String json, Class<T> type) {
-        return decode(json, type, null);
-    }
+	/**
+	 * 将JSON字符串解码为JAVA对象
+	 * @param <T> 对象泛型
+	 * @param json 对象的JSON字符串
+	 * @param type JAVA对象类型
+	 * @return JSON对应的JAVA对象，如果无法解析将返回NULL.
+	 */
+	public <T> T decode(String json, Class<T> type) {
+		return decode(json, type, null);
+	}
 
-    /**
-     * 将JSON字符串，解码为JAVA对象
-     * @param <T> 对象泛型
-     * @param json JSON字符串
-     * @param type JAVA对象类型
-     * @param defaultValue 默认值
-     * @return JSON对应的JAVA对象，如果无法解析将返回默认值.
-     */
-    public <T> T decode(String json, Class<T> type, T defaultValue) {
-        try {
-            return gson.fromJson(json, type);
-        } catch (Exception e) {
-            logger.warn("#", e);
-            return defaultValue;
-        }
-    }
+	/**
+	 * 将JSON字符串，解码为JAVA对象
+	 * @param <T> 对象泛型
+	 * @param json JSON字符串
+	 * @param type JAVA对象类型
+	 * @param defaultValue 默认值
+	 * @return JSON对应的JAVA对象，如果无法解析将返回默认值.
+	 */
+	public <T> T decode(String json, Class<T> type, T defaultValue) {
+		try {
+			return gson.fromJson(json, type);
+		} catch (Exception e) {
+			logger.warn("#", e);
+			return defaultValue;
+		}
+	}
 
-    /**
-     * 将JSON字符串，解码为JAVA对象
-     * @param <T> 对象泛型
-     * @param json JSON字符串
-     * @param token 类型标记
-     * @return JSON对应的JAVA对象，如果无法解析将返回默认值.
-     */
-    public <T> T decode(String json, TypeReference<T> token) {
-        try {
-            Type type = token.getType();
-            return gson.fromJson(json, type);
-        } catch (Throwable e) {
-            logger.error("#", e);
-            return null;
-        }
-    }
+	/**
+	 * 将JSON字符串，解码为JAVA对象
+	 * @param <T> 对象泛型
+	 * @param json JSON字符串
+	 * @param token 类型标记
+	 * @return JSON对应的JAVA对象，如果无法解析将返回默认值.
+	 */
+	public <T> T decode(String json, TypeReference<T> token) {
+		try {
+			Type type = token.getType();
+			return gson.fromJson(json, type);
+		} catch (Throwable e) {
+			logger.error("#", e);
+			return null;
+		}
+	}
 
-    /**
-     * 将JSON字符串，解码为Map对象
-     * @param json JSON字符串
-     * @return JSON对应的Map对象，如果无法解析将返回NULL.
-     */
-    @Override
-    public Mapx toMap(String json) {
-        try {
-            JsonElement node = new JsonParser().parse(json);
-            return GsonConvertUtil.toMap(node);
-        } catch (Exception e) {
-            logger.error("#", e);
-            return null;
-        }
-    }
+	/**
+	 * 将JSON字符串，解码为Map对象
+	 * @param json JSON字符串
+	 * @return JSON对应的Map对象，如果无法解析将返回NULL.
+	 */
+	@Override
+	public Mapx toMap(String json) {
+		try {
+			@SuppressWarnings("deprecation")
+			JsonElement node = new JsonParser().parse(json);
+			return GsonConvertUtil.toMap(node);
+		} catch (Exception e) {
+			logger.error("#", e);
+			return null;
+		}
+	}
 
-    /**
-     * 将JSON字符串，解码为List对象
-     * @param json JSON字符串
-     * @return JSON对应的List对象，如果无法解析将返回NULL.
-     */
-    @Override
-    public Listx toList(String json) {
-        try {
-            JsonElement node = new JsonParser().parse(json);
-            return GsonConvertUtil.toList(node);
-        } catch (Exception e) {
-            logger.error("#", e);
-            return null;
-        }
-    }
+	/**
+	 * 将JSON字符串，解码为List对象
+	 * @param json JSON字符串
+	 * @return JSON对应的List对象，如果无法解析将返回NULL.
+	 */
+
+	@Override
+	public Listx toList(String json) {
+		try {
+			@SuppressWarnings("deprecation")
+			JsonElement node = new JsonParser().parse(json);
+			return GsonConvertUtil.toList(node);
+		} catch (Exception e) {
+			logger.error("#", e);
+			return null;
+		}
+	}
 }
