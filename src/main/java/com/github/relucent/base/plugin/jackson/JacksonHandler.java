f42a40daea4ec5eb3d54a85873e6a22d9aa35e8b
yyl <relucent@163.com>
2022-11-24 20:18:59 +0800

    +

--- src/main/java/com/github/relucent/base/plugin/jackson/JacksonHandler.java
+++ src/main/java/com/github/relucent/base/plugin/jackson/JacksonHandler.java
@@ -1,7 +1,9 @@
 package com.github.relucent.base.plugin.jackson;
 
 import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.github.relucent.base.common.collection.Listx;
 import com.github.relucent.base.common.collection.Mapx;
@@ -9,118 +11,188 @@ import com.github.relucent.base.common.json.JsonHandler;
 import com.github.relucent.base.common.logging.Logger;
 import com.github.relucent.base.common.reflect.TypeReference;
 
+/**
+ * JSON处理器 （基于JACKSON实现）
+ */
 public class JacksonHandler implements JsonHandler {
 
-    // ===================================Fields==============================================
-    public static final JacksonHandler INSTANCE = new JacksonHandler();
-    private final Logger logger = Logger.getLogger(getClass());
-    private final ObjectMapper objectMapper;
+	// ===================================Fields==============================================
+	public static final JacksonHandler INSTANCE = new JacksonHandler();
+	private final Logger logger = Logger.getLogger(getClass());
+	private final ObjectMapper objectMapper;
+	private final ObjectWriter prettyWriter;
 
-    // ===================================Constructors========================================
-    /**
-     * 构造函数(使用指定 {@link ObjectMapper})
-     * @param objectMapper 对象映射
-     */
-    public JacksonHandler(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
+	// ===================================Constructors========================================
+	/**
+	 * 构造函数(使用指定 {@link ObjectMapper})
+	 * @param objectMapper 对象映射
+	 */
+	public JacksonHandler(ObjectMapper objectMapper) {
+		this.objectMapper = objectMapper;
+		this.prettyWriter = objectMapper.writerWithDefaultPrettyPrinter();
+	}
 
-    /**
-     * 构造函数(默认)
-     */
-    public JacksonHandler() {
-        this(MyObjectMapper.INSTANCE);
-    }
+	/**
+	 * 构造函数(默认)
+	 */
+	public JacksonHandler() {
+		this(MyObjectMapper.INSTANCE);
+	}
 
-    // ===================================Methods=============================================
+	// ===================================Methods=============================================
 
-    /**
-     * 将JAVA对象编码为JSON字符串
-     * @param src JAVA对象
-     * @return 对象的JSON字符串
-     */
-    public String encode(Object src) {
-        try {
-            return objectMapper.writeValueAsString(src);
-        } catch (Throwable e) {
-            logger.warn("#", e);
-            return null;
-        }
-    }
+	/**
+	 * 将JAVA对象编码为JSON字符串
+	 * @param src JAVA对象
+	 * @return 对象的JSON字符串
+	 */
+	@Override
+	public String encode(Object src) {
+		try {
+			return objectMapper.writeValueAsString(src);
+		} catch (Throwable e) {
+			logger.warn("#", e);
+			return null;
+		}
+	}
 
-    /**
-     * 将JSON字符串解码为JAVA对象
-     * @param <T> 对象泛型
-     * @param json 对象的JSON字符串
-     * @param type JAVA对象类型
-     * @return JSON对应的JAVA对象，如果无法解析将返回NULL.
-     */
-    public <T> T decode(String json, Class<T> type) {
-        return decode(json, type, null);
-    }
+	/**
+	 * 将JAVA对象编码为带缩进打印格式的JSON字符串对象
+	 * @param value JAVA对象
+	 * @return 对象的JSON字符串
+	 */
+	public String encodePretty(Object value) {
+		try {
+			return prettyWriter.writeValueAsString(value);
+		} catch (Exception e) {
+			throw new IllegalStateException(e);
+		}
+	}
 
-    /**
-     * 将JSON字符串，解码为JAVA对象
-     * @param <T> 对象泛型
-     * @param json JSON字符串
-     * @param type JAVA对象类型
-     * @param defaultValue 默认值
-     * @return JSON对应的JAVA对象，如果无法解析将返回默认值.
-     */
-    public <T> T decode(String json, Class<T> type, T defaultValue) {
-        try {
-            return objectMapper.readValue(json, type);
-        } catch (Throwable e) {
-            logger.warn("#", e);
-            return defaultValue;
-        }
-    }
+	/**
+	 * 将JSON字符串解码为JSON对象
+	 * @param json 对象的JSON字符串
+	 * @return JSON对象，如果无法解析将返回NULL
+	 */
+	public JsonNode decode(String json) {
+		try {
+			return objectMapper.readValue(json, JsonNode.class);
+		} catch (Exception e) {
+			logger.warn("#", e);
+			return null;
+		}
+	}
 
-    /**
-     * 将JSON字符串，解码为JAVA对象
-     * @param <T> 对象泛型
-     * @param json JSON字符串
-     * @param token 类型标记
-     * @return JSON对应的JAVA对象，如果无法解析将返回默认值.
-     */
-    public <T> T decode(String json, TypeReference<T> token) {
-        try {
-            TypeFactory typeFactory = objectMapper.getTypeFactory();
-            JavaType valueType = typeFactory.constructType(token.getType());
-            return objectMapper.readValue(json, valueType);
-        } catch (Throwable e) {
-            logger.error("#", e);
-            return null;
-        }
-    }
+	/**
+	 * 将JSON字符串解码为JAVA对象
+	 * @param <T> 对象泛型
+	 * @param json 对象的JSON字符串
+	 * @param type JAVA对象类型
+	 * @return JSON对应的JAVA对象，如果无法解析将返回NULL
+	 */
+	@Override
+	public <T> T decode(String json, Class<T> type) {
+		return decode(json, type, null);
+	}
 
-    /**
-     * 将JSON字符串，解码为Map对象(该方法依赖于JACKSON类库)
-     * @param json JSON字符串
-     * @return JSON对应的Map对象，如果无法解析将返回NULL.
-     */
-    @Override
-    public Mapx toMap(String json) {
-        try {
-            return JacksonConvertUtil.toMap(objectMapper.readTree(json));
-        } catch (Throwable e) {
-            logger.error("#", e);
-            return null;
-        }
-    }
+	/**
+	 * 将JSON字符串，解码为JAVA对象
+	 * @param <T> 对象泛型
+	 * @param json JSON字符串
+	 * @param type JAVA对象类型
+	 * @param defaultValue 默认值
+	 * @return JSON对应的JAVA对象，如果无法解析将返回默认值.
+	 */
+	public <T> T decode(String json, Class<T> type, T defaultValue) {
+		try {
+			return objectMapper.readValue(json, type);
+		} catch (Throwable e) {
+			logger.warn("#", e);
+			return defaultValue;
+		}
+	}
 
-    /**
-     * 将JSON字符串，解码为List对象
-     * @param json JSON字符串
-     * @return JSON对应的List对象，如果无法解析将返回NULL.
-     */
-    @Override
-    public Listx toList(String json) {
-        try {
-            return JacksonConvertUtil.toList(objectMapper.readTree(json));
-        } catch (Throwable e) {
-            logger.error("#", e);
-            return null;
-        }
-    }
+	/**
+	 * 将JSON字符串，解码为JAVA对象
+	 * @param <T> 对象泛型
+	 * @param json JSON字符串
+	 * @param token 类型标记
+	 * @return JSON对应的JAVA对象，如果无法解析将返回默认值.
+	 */
+	@Override
+	public <T> T decode(String json, TypeReference<T> token) {
+		try {
+			TypeFactory typeFactory = objectMapper.getTypeFactory();
+			JavaType valueType = typeFactory.constructType(token.getType());
+			return objectMapper.readValue(json, valueType);
+		} catch (Throwable e) {
+			logger.error("#", e);
+			return null;
+		}
+	}
+
+	/**
+	 * 将JSON字符串，解码为Map对象(该方法依赖于JACKSON类库)
+	 * @param json JSON字符串
+	 * @return JSON对应的Map对象，如果无法解析将返回NULL.
+	 */
+	@Override
+	public Mapx toMap(String json) {
+		try {
+			return JacksonConvertUtil.toMap(objectMapper.readTree(json));
+		} catch (Throwable e) {
+			logger.error("#", e);
+			return null;
+		}
+	}
+
+	/**
+	 * 将JSON字符串，解码为List对象
+	 * @param json JSON字符串
+	 * @return JSON对应的List对象，如果无法解析将返回NULL.
+	 */
+	@Override
+	public Listx toList(String json) {
+		try {
+			return JacksonConvertUtil.toList(objectMapper.readTree(json));
+		} catch (Throwable e) {
+			logger.error("#", e);
+			return null;
+		}
+	}
+
+	/**
+	 * 将JSON对象转化为指定类型的JAVA对象
+	 * @param <T> JAVA对象泛型
+	 * @param node JSON对象
+	 * @param type JAVA对象类型
+	 * @return 转换后的JAVA对象
+	 */
+	public <T> T convertValue(JsonNode node, Class<T> type) {
+		if (node == null) {
+			return null;
+		}
+		return objectMapper.convertValue(node, type);
+	}
+
+	/**
+	 * 将JSON对象转化为指定类型的JAVA对象
+	 * @param <T> JAVA对象泛型
+	 * @param node JSON对象
+	 * @param token 类型标记
+	 * @return 转换后的JAVA对象
+	 */
+	public <T> T convertValue(JsonNode node, TypeReference<T> token) {
+		if (node == null) {
+			return null;
+		}
+		try {
+			TypeFactory typeFactory = objectMapper.getTypeFactory();
+			JavaType valueType = typeFactory.constructType(token.getType());
+			return objectMapper.convertValue(node, valueType);
+		} catch (Throwable e) {
+			logger.error("#", e);
+			return null;
+		}
+	}
 }
--- src/main/java/com/github/relucent/base/plugin/jackson/MyObjectMapper.java
+++ src/main/java/com/github/relucent/base/plugin/jackson/MyObjectMapper.java
@@ -24,38 +24,38 @@ import com.github.relucent.base.plugin.jackson.databind.DatePowerSerializer;
 @SuppressWarnings("serial")
 public class MyObjectMapper extends ObjectMapper {
 
-    public static final MyObjectMapper INSTANCE = new MyObjectMapper();
+	public static final MyObjectMapper INSTANCE = new MyObjectMapper();
 
-    public MyObjectMapper() {
-        initialize();
-    }
+	public MyObjectMapper() {
+		initialize();
+	}
 
-    protected void initialize() {
-        // 当找不到对应的序列化器时 忽略此字段
-        this.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+	protected void initialize() {
+		// 当找不到对应的序列化器时 忽略此字段
+		this.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
 
-        // 支持结束
-        this.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        this.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
-        //
-        this.setDateFormat(new SimpleDateFormat(DateUtil.DATETIME_FORMAT));
-        // 反序列化忽略不需要的字段
-        this.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		// 支持结束
+		this.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+		this.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+		//
+		this.setDateFormat(new SimpleDateFormat(DateUtil.DATETIME_FORMAT));
+		// 反序列化忽略不需要的字段
+		this.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-        SimpleModule module = new SimpleModule();
+		SimpleModule module = new SimpleModule();
 
-        // 将大数字转换为 String 类型
-        module.addSerializer(BigDecimal.class, BigDecimalPowerSerializer.INSTANCE);
-        module.addDeserializer(BigDecimal.class, BigDecimalPowerDeserializer.INSTANCE);
+		// 将大数字转换为 String 类型
+		module.addSerializer(BigDecimal.class, BigDecimalPowerSerializer.INSTANCE);
+		module.addDeserializer(BigDecimal.class, BigDecimalPowerDeserializer.INSTANCE);
 
-        // 日期序列化与反序列化
-        module.addSerializer(Date.class, DatePowerSerializer.INSTANCE);
-        module.addDeserializer(Date.class, DatePowerDeserializer.INSTANCE);
+		// 日期序列化与反序列化
+		module.addSerializer(Date.class, DatePowerSerializer.INSTANCE);
+		module.addDeserializer(Date.class, DatePowerDeserializer.INSTANCE);
 
-        // 扩展集合类反序列化
-        module.addDeserializer(Mapx.class, JacksonConvertUtil.MAP_DESERIALIZER);
-        module.addDeserializer(Listx.class, JacksonConvertUtil.LIST_DESERIALIZER);
+		// 扩展集合类反序列化
+		module.addDeserializer(Mapx.class, JacksonConvertUtil.MAP_DESERIALIZER);
+		module.addDeserializer(Listx.class, JacksonConvertUtil.LIST_DESERIALIZER);
 
-        this.registerModule(module);
-    }
+		this.registerModule(module);
+	}
 }
