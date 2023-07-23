ff2e0f05635b3240df0d685f62568b41485054f6
yyl <relucent@163.com>
2023-07-23 12:28:29 +0800

    +

--- src/main/java/com/github/relucent/base/common/awt/ScreenUtil.java
+++ src/main/java/com/github/relucent/base/common/awt/ScreenUtil.java
@@ -1,6 +1,7 @@
 package com.github.relucent.base.common.awt;
 
 import java.awt.Dimension;
+import java.awt.Frame;
 import java.awt.Rectangle;
 import java.awt.Toolkit;
 
@@ -30,5 +31,14 @@ public class ScreenUtil {
     public static Rectangle getRectangle() {
         Dimension dimension = getScreenSize();
         return new Rectangle(dimension.width, dimension.height);
+    }
+
+    /**
+     * 设置窗口对象(Frame)屏幕居中
+     * @param frame 窗口对象
+     */
+    public static void setCenterLocation(Frame frame) {
+        Dimension screenSize = getScreenSize();
+        frame.setLocation((screenSize.width - frame.getWidth()) / 2, (screenSize.height - frame.getHeight()) / 2);
     }
 }
