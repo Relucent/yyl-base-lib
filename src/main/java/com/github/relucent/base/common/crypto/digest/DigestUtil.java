d0a4cc12fa0e067b6c09d668d251b06633de4523
yyl <relucent@163.com>
2023-12-15 10:36:55 +0800

    #

--- src/main/java/com/github/relucent/base/common/crypto/digest/DigestUtil.java
+++ src/main/java/com/github/relucent/base/common/crypto/digest/DigestUtil.java
@@ -5,19 +5,37 @@ package com.github.relucent.base.common.crypto.digest;
  */
 public class DigestUtil {
 
-    public static byte[] md5(byte[] input) {
-        return Md5.create().digest(input);
-    }
+	// =================================MD5====================================================
+	public static byte[] md5(byte[] input) {
+		return Md5.create().digest(input);
+	}
 
-    public static byte[] md5(String input) {
-        return Md5.create().digest(input);
-    }
+	public static byte[] md5(String input) {
+		return Md5.create().digest(input);
+	}
 
-    public static String md5Hex(String input) {
-        return Md5.create().digestHex(input);
-    }
+	public static String md5Hex(String input) {
+		return Md5.create().digestHex(input);
+	}
 
-    public static String md5Hex(byte[] input) {
-        return Md5.create().digestHex(input);
-    }
+	public static String md5Hex(byte[] input) {
+		return Md5.create().digestHex(input);
+	}
+
+	// =================================SM3====================================================
+	public static byte[] sm3(byte[] input) {
+		return Sm3.create().digest(input);
+	}
+
+	public static byte[] sm3(String input) {
+		return Sm3.create().digest(input);
+	}
+
+	public static String sm3Hex(String input) {
+		return Sm3.create().digestHex(input);
+	}
+
+	public static String sm3Hex(byte[] input) {
+		return Sm3.create().digestHex(input);
+	}
 }
